4b45d6972537711db45662bae7e904454f358ed8
rajbhati <bhati.raj@gmail.com>
2018-02-14 20:27:05 +0530

    backtracking in partitionset_equalSubsets for printing sets

--- dynamic_programming/partitionSet_equalSubsets.cpp
+++ dynamic_programming/partitionSet_equalSubsets.cpp
@@ -1,3 +1,4 @@
+
 /* 
  * Given a set S of positive integers, partition S into two subsets S1 and S2 such that:
  * 1. S1 and S2 disjoint. (they don't share any element)
@@ -17,68 +18,13 @@
 
 using namespace std;
 
-/*
- * subsetsum(arr, n, sum) =
- * 	subsetsum(arr, n-1, sum) ||
- * 	subsetsum(arr, n-1, sum-arr[n-1])
- *
- * 	Base case:
- * 	subsetsum(arr, n, sum) = false, if sum>0 and n=0
- * 	subsetsum(arr, n, sum) = true, if sum=0
- */
-multiset<int> subsetsumDP(vector<int> &arr, int sum) {
-    /* ss[i][j] = true, if subsetsumDP(arr, i, j) is true
-     * i.e. there is a subset of arr[0] .. arr[i-1] with sum j 
-     */
-    size_t n = arr.size();
-    vector<vector<bool>> ssDP(n+1, vector<bool>(sum+1, false)); 
-
-    // if sum=0, there's always a subset
-    for_each(ssDP.begin(), ssDP.end(), [](vector<bool> &row){row[0]=true;});
-    
-    // if sum!=0, and set is empty, then there's no subset possible
-    for_each(next(ssDP[0].begin()), ssDP[0].end(), [](_Bit_reference x){x=false;});
-
-    for (int row = 1; row <= n; ++row){
-        for (int col = 1; col <= sum; ++col) {
-            if (col < arr[row - 1]) { // sum is less than current number being considered
-                ssDP[row][col] = ssDP[row-1][col];  // current number can't be used to make sum
-            } else {
-                ssDP[row][col] = ssDP[row-1][col - arr[row-1]] || // include current element in the sum 'col'
-                        ssDP[row-1][col]; // find sum 'col' without including this element
-            }
-        }
-    }
-
-    multiset<int> sumSet;
-    
-    if (ssDP[n][sum]) { // subset found
-
-        int remaining = sum;
-        int row = n;
-        while(remaining){
-            for (; row > 0; --row) {
-                if (ssDP[row][remaining] && !ssDP[row-1][remaining]) { // can't make sum without current element
-                    sumSet.insert(arr[row-1]);
-                    remaining = remaining - arr[row-1];
-                    if (!remaining)
-                        return sumSet;
-                    break;
-                }
-            }
-        }
-    }
-
-    return sumSet;
-}
-
 /* 
  * DP(S(n), X, Y) = Solution to setPartition n element set S(n), partitioned into two sets with sum X and Y.
  *
- * DP[S(n), X, Y] = DP[S(n-1), X, Y] or 			{don't use nth item}
- * 					DP[S(n-1), X - value(n), Y] or	{use nth item in X}
- * 					DP[S(n-1), X, Y - value(n)];	{use nth item in Y}
- * 						
+ * DP[S(n), X, Y] = DP[S(n-1), X, Y] or                         {don't use nth item}
+ *                                      DP[S(n-1), X - value(n), Y] or  {use nth item in X}
+ *                                      DP[S(n-1), X, Y - value(n)];    {use nth item in Y}
+ * 
  */
 bool partitionSet(vector<int> &arr, int sum) {
 
@@ -92,7 +38,7 @@ bool partitionSet(vector<int> &arr, int sum) {
             }
         }
 
-	for(int i=0; i<=n; ++i) // subsets of size '0' are always possible
+        for(int i=0; i<=n; ++i) // subsets of size '0' are always possible
         ss[i][0][0] = true;
 
     for (int items = 1; items <= n; ++items){
@@ -118,33 +64,27 @@ bool partitionSet(vector<int> &arr, int sum) {
     for(int i=sum; i>0; --i){
         if(ss[n][i][i]) { // maximum sum subset
             cout << endl << "Maximum sum: " << i << endl;
-    		multiset<int> setX; // first set, X
-            int remaining = i;
-            int row = n;
-            while(remaining){
-                int xx = remaining;
-                for(; row>0; --row){
-                    if(ss[row][xx][0] && !ss[row-1][xx][0]){
-                        setX.insert(arr[row-1]);
-                        remaining -= arr[row-1];
-                        break;
+            multiset<int> setx;
+            multiset<int> sety;
+            int rx, ry;
+            rx = ry = i;
+            for(; n>0 && (rx || ry); --n ){
+//                cout << endl << "rx: " << rx << ", ry: " << ry << ", considering: " << arr[n-1] << endl;
+                if(ss[n][rx][ry] && !ss[n-1][rx][ry]){ // arr[n-1] is essential for either x or y
+                    if(rx && (arr[n-1] <= rx) && !ss[n-1][rx][ ry-arr[n-1] ]){ // put in X
+                        rx -= arr[n-1];
+                        setx.insert(arr[n-1]);
+                    } else {
+                        ry -= arr[n-1];
+                        sety.insert(arr[n-1]);
                     }
                 }
             }
-            
-            cout << "First set: ";
-            for_each(setX.begin(), setX.end(), [](int x){cout << x << ",";});
-            cout << endl;
 
-			// use subsetSumDP to find a subset of 'i' sum in remaining elements of arr
-            vector<int> yarr; 
-			sort(arr.begin(), arr.end()); // need to sort for set_difference
-            set_difference(arr.begin(), arr.end(), setX.begin(), setX.end(), inserter(yarr, yarr.begin()));
-
-            multiset<int> setY = subsetsumDP(yarr, i);
+            cout << "First set : ";
+            for_each(setx.begin(), setx.end(), [](int x){cout << x << ",";}); cout << endl;
             cout << "Second set: ";
-            for_each(setY.begin(), setY.end(), [](int x){cout << x << ",";});
-            cout << endl;
+            for_each(sety.begin(), sety.end(), [](int x){cout << x << ",";}); cout << endl;
 
             return true;
         }
@@ -168,19 +108,18 @@ int main() {
 }
 
 void prettyPrint(vector<vector<bool>> &vv, vector<int> &arr){
-	cout << endl << __PRETTY_FUNCTION__ << endl;
-	cout << "  0 ";
-	for (int j = 1; j < vv[0].size(); j++)
-		cout <<  j << " ";
-	cout << endl;
+        cout << endl << __PRETTY_FUNCTION__ << endl;
+        cout << "  0 ";
+        for (int j = 1; j < vv[0].size(); j++)
+                cout <<  j << " ";
+        cout << endl;
 
-	for (int i = 0; i < vv.size(); ++i) {
-		if (i > 0) cout << arr[i - 1] << " "; 
-		else cout << "_ "; 
-		for (int j = 0; j < vv[0].size(); ++j)
-			cout << vv[i][j] << " ";
-		cout << endl ;
-	}
-	cout << endl;
+        for (int i = 0; i < vv.size(); ++i) {
+                if (i > 0) cout << arr[i - 1] << " "; 
+                else cout << "_ "; 
+                for (int j = 0; j < vv[0].size(); ++j)
+                        cout << vv[i][j] << " ";
+                cout << endl ;
+        }
+        cout << endl;
 }
-
