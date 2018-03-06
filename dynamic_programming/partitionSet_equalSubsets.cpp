d52ee49ec7d240039a72bada42bb1799722b8564
rajbhati <bhati.raj@gmail.com>
2018-03-06 18:09:33 +0530

    minor

---
+++ dynamic_programming/partitionSet_equalSubsets.cpp
@@ -0,0 +1,111 @@
+
+/* 
+ * Given a set S of positive integers, partition S into two subsets S1 and S2 such that:
+ * 1. S1 and S2 disjoint. (they don't share any element)
+ * 2. Sum of all elements in S1 = Sum of all elements in S2
+ * 3. The above Sum is the maximum such sum possible.
+ * 
+ * You need not use all elements in S to create S1 and S2, in other words you can discard elements
+ * from S (i.e. S1 U S2 != S)
+ * 
+ * Output the maximal sum.
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+/* 
+ * DP(S(n), X, Y) = Solution to setPartition n element set S(n), partitioned into two sets with sum X and Y.
+ *
+ * DP[S(n), X, Y] = DP[S(n-1), X, Y] or                         {don't use nth item}
+ *                                      DP[S(n-1), X - value(n), Y] or  {use nth item in X}
+ *                                      DP[S(n-1), X, Y - value(n)];    {use nth item in Y}
+ * 
+ */
+bool partitionSet(vector<int> &arr, int sum) {
+
+    size_t n = arr.size();
+
+    bool ss[n+1][sum+1][sum+1];
+    for(int i=0; i<=n; ++i){
+        for(int j=0; j<=sum; ++j){
+            for(int k=0; k<=sum; ++k){
+                ss[i][j][k] = false; 
+            }
+        }
+    }
+
+    for(int i=0; i<=n; ++i) ss[i][0][0] = true; // subsets of size '0' are always possible
+
+    for (int item = 1; item <= n; ++item){
+        for (int x = 0; x<=sum; ++x) {
+            for(int y=0; y<=sum; ++y){
+                if (x < arr[item-1] && y < arr[item-1]) { // x and y are less than the item being considered
+                    ss[item][x][y] = ss[item-1][x][y];    // current item can't be used in x or y
+                } else if (x < arr[item-1]) { // current item can be used to make y
+                    ss[item][x][y] = ss[item-1][x][y] ||              // don't use item  
+                                      ss[item-1][x][y-arr[item-1]];   // use item for y
+                } else if (y < arr[item - 1]) { // current item can be used to make x
+                    ss[item][x][y] = ss[item-1][x][y] ||              // don't use item  
+                                      ss[item-1][x-arr[item-1]][y];   // use item for x
+                } else { // current item can be used to make either x or y
+                    ss[item][x][y] = ss[item-1][x][y] ||              // don't use item
+                                      ss[item-1][x-arr[item-1]][y] || // use item for x
+                                      ss[item-1][x][y-arr[item-1]];   // use item for y
+                }
+            }
+        }
+    }
+
+    for(int i=sum; i>0; --i){
+        if(ss[n][i][i]) { // maximum sum subset
+            cout << endl << "Maximum sum: " << i << endl;
+            multiset<int> setx;
+            multiset<int> sety;
+            int rx, ry; // remaining values of sum X, sum Y, while backtracking for set X and Y
+            rx = ry = i;
+            for(; n>0 && (rx || ry); --n ){ // consider items one by one
+                if(ss[n][rx][ry] && !ss[n-1][rx][ry]){ // arr[n-1] is essential for either x or y
+                    if(rx && (arr[n-1] <= rx) && !ss[n-1][rx][ ry-arr[n-1] ]){ 
+                    // without the nth element, sumSets {rx} and {ry - arr[n-1]} would be impossible. 
+                    // i.e. it's not possible to put arr[n-1] in set Y, hence it goes in X
+                        rx -= arr[n-1];
+                        setx.insert(arr[n-1]);
+                    } else {
+                        ry -= arr[n-1];
+                        sety.insert(arr[n-1]);
+                    }
+                }
+            }
+
+            cout << "First set: ";
+            for(auto x : setx) cout << x << ","; cout << endl;
+            cout << "Second set: ";
+            for(auto x : sety) cout << x << ","; cout << endl;
+
+            return true;
+        }
+    }
+    
+    cout << "No equal sum sets possible!" << endl;
+
+    return false;
+}
+
+int main() {
+    int sum, n, i;
+    
+    cout << "Input set size: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Input " << n << " numbers (space separated): ";
+    for (int i=0; i<n; ++i)
+        cin >> arr[i];
+
+    sum = accumulate(arr.begin(), arr.end(), 0) / 2;
+    partitionSet(arr, sum);
+}
